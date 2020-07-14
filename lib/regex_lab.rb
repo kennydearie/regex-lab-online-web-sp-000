bc3c7e500e375d1099f3f30865f32bd79ae6b86b
Nkenge Simms <simmsn@yahoo.com>
2020-07-14 16:02:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -28,7 +28,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 
 def valid_phone_number?(phone) 
   binding.pry
- if phone.match(/(\d){10}|(\((\d){3}\))((\d){7}|(\d){3}-(\d){4})/)
+ if phone.match(/(\d){10}|(\((\d){3}\))((\d){7}|(\d){3}-(\d){4})|(\d){3} (\d){3} (\d){4}/)
    return true
  else 
    return false
